67fc7ce88e84230ee30344ac181a5fc637ecc925
JustineDevs <pcg0255@gmail.com>
2025-09-01 18:40:30 +0800

    app: wrapper

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import '../src/index.css'
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
+import Sidebar from '../src/components/Sidebar'
+
+function MyApp({ Component, pageProps }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  return (
+    <div className="flex h-screen bg-gray-50">
+      {/* Mobile menu button */}
+      <div className="lg:hidden fixed top-4 left-4 z-50">
+        <button
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          className="bg-white p-2 rounded-lg shadow-md border border-gray-200 hover:bg-gray-50"
+        >
+          {sidebarOpen ? (
+            <X className="h-6 w-6 text-gray-600" />
+          ) : (
+            <Menu className="h-6 w-6 text-gray-600" />
+          )}
+        </button>
+      </div>
+
+      {/* Mobile overlay */}
+      {sidebarOpen && (
+        <div 
+          className="lg:hidden fixed inset-0 z-40 bg-black bg-opacity-50"
+          onClick={() => setSidebarOpen(false)}
+        />
+      )}
+
+      <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+      <main className="flex-1 overflow-auto lg:ml-0">
+        <div className="lg:hidden h-16"></div> {/* Spacer for mobile menu button */}
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
+}
+
+export default MyApp

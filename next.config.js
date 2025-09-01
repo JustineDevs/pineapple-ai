fbdcb72e769a2c7029e93493b6db1c652e542d3f
JustineDevs <pcg0255@gmail.com>
2025-09-01 20:24:20 +0800

    finding fix issue

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'pineapple-ai.vercel.app'],
+    domains: ['localhost'],
   },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,

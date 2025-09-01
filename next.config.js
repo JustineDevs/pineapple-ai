78f9ca1f313c8f0279546ac7c22300a9d96b8197
JustineDevs <pcg0255@gmail.com>
2025-09-01 20:03:58 +0800

    fix: critical-deployment-issues

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'pineapple-ai.vercel.app'],
   },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
